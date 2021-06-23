8b977aa2071ef8b5b4596d2805ede18af79fef2d
Ryan Bab <ryanbaba91@gmail.com>
2021-06-23 10:16:17 +0530

    Adding remove logic in child

--- force-app/main/default/aura/d2_child/d2_childController.js
+++ force-app/main/default/aura/d2_child/d2_childController.js
@@ -1,13 +1,12 @@
 ({
     removeItem : function(component, event, helper) {
-        console.log('1.');
 
-        let itemIndex = component.get('c.indexNum');
-        console.log('2..');
-
-        // var indexPosition = event.target.name;
-        let eduList = component.get('c.eduList');
-        eduList.splice(itemIndex);
-        component.set('c.eduList',eduList);
+        let itemIndex = component.get('v.indexNo');
+        let eduList = component.get('v.eduList2');
+        if(itemIndex>-1){
+            eduList.splice(itemIndex,1);
+        }
+        component.set('v.eduList2',eduList);
     }
+    
 })
